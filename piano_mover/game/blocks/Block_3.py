27d34271e388d015221290db11145b1d22870c16
ricdip <riccardodpr@gmail.com>
2021-08-03 12:11:46 +0200

    piano mover: block 3 moves

--- piano_mover/game/blocks/Block_3.py
+++ piano_mover/game/blocks/Block_3.py
@@ -1,23 +1,93 @@
 import random
+from copy import deepcopy
+import game
 
 # 3 obstacles
 class Block_3:
-    def __init__(self, id=None, representation=None):
+    def __init__(self, id=None, representation=None, pos=None):
         self.id = id
         self.repr = representation
-        self.pos = []
-        self.value = 3
-        self.head_x = None
-        self.head_y = None
-        self.body_1_x = None
-        self.body_1_y = None
-        self.body_2_x = None
-        self.body_2_y = None
 
-        self.__generate_positions()
+        if pos is None:
+            self.pos = []
+            self.__generate_positions()
+        else:
+            self.pos = pos
+            self.head_x = pos[0][0]
+            self.head_y = pos[0][1]
+            self.body_1_x = pos[1][0]
+            self.body_1_y = pos[1][1]
+            self.body_2_x = pos[2][0]
+            self.body_2_y = pos[2][1]
+
+        self.value = 3
 
     def move_up(self):
-        pass
+        new_head_x = self.head_x - 1
+        new_head_y = self.head_y
+        new_body_1_x = self.body_1_x - 1
+        new_body_1_y = self.body_1_y
+        new_body_2_x = self.body_2_x - 1
+        new_body_2_y = self.body_2_y
+        new_pos = []
+        new_pos.append((new_head_x, new_head_y))
+        new_pos.append((new_body_1_x, new_body_1_y))
+        new_pos.append((new_body_2_x, new_body_2_y))
+
+        return self.__move(new_pos)
+
+    def move_down(self):
+        new_head_x = self.head_x + 1
+        new_head_y = self.head_y
+        new_body_1_x = self.body_1_x + 1
+        new_body_1_y = self.body_1_y
+        new_body_2_x = self.body_2_x + 1
+        new_body_2_y = self.body_2_y
+        new_pos = []
+        new_pos.append((new_head_x, new_head_y))
+        new_pos.append((new_body_1_x, new_body_1_y))
+        new_pos.append((new_body_2_x, new_body_2_y))
+
+        return self.__move(new_pos)
+
+    def move_left(self):
+        new_head_x = self.head_x
+        new_head_y = self.head_y - 1
+        new_body_1_x = self.body_1_x
+        new_body_1_y = self.body_1_y - 1
+        new_body_2_x = self.body_2_x
+        new_body_2_y = self.body_2_y - 1
+        new_pos = []
+        new_pos.append((new_head_x, new_head_y))
+        new_pos.append((new_body_1_x, new_body_1_y))
+        new_pos.append((new_body_2_x, new_body_2_y))
+
+        return self.__move(new_pos)
+
+    def move_right(self):
+        new_head_x = self.head_x
+        new_head_y = self.head_y + 1
+        new_body_1_x = self.body_1_x
+        new_body_1_y = self.body_1_y + 1
+        new_body_2_x = self.body_2_x
+        new_body_2_y = self.body_2_y + 1
+        new_pos = []
+        new_pos.append((new_head_x, new_head_y))
+        new_pos.append((new_body_1_x, new_body_1_y))
+        new_pos.append((new_body_2_x, new_body_2_y))
+
+        return self.__move(new_pos)
+
+    def __move(self, new_pos):
+        if not self.__collision(new_pos):
+            new_blocks = deepcopy(self.repr.blocks)
+            new_repr = game.PianoMoverRepresentation(
+                dim=self.repr.dim, blocks=new_blocks
+            )
+            new_repr.update_block(Block_3(self.id, new_repr, new_pos))
+
+            return new_repr
+        return None
 
     def __generate_positions(self):
         while True:
@@ -101,5 +171,24 @@ class Block_3:
         self.pos.append((self.body_1_x, self.body_1_y))
         self.pos.append((self.body_2_x, self.body_2_y))
 
-    def __update_repr(self):
-        self.repr.update_grid()
+    def __collision(self, new_pos):
+        for pos in new_pos:
+            if (
+                pos[0] < 0
+                or pos[0] > self.repr.get_grid_shape()[0] - 1
+                or pos[1] < 0
+                or pos[1] > self.repr.get_grid_shape()[1] - 1
+                or self.__block_collision(new_pos)
+            ):
+                return True
+
+        return False
+
+    def __block_collision(self, new_pos):
+        for k in self.repr.blocks:
+            if self.repr.blocks[k].id != "exit" and self.repr.blocks[k].id != self.id:
+                for pos in self.repr.blocks[k].pos:
+                    if pos in new_pos:
+                        return True
+
+        return False
